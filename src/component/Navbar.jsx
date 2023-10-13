1280de7c227963b9c336eeb7a3ed3149ed05c9f2
Ravi Kumar <ravi.kumar4rawat@gmail.com>
2023-10-13 12:25:49 +0530

    done

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import './navbar.css';
 
 const Navbar = () => {
+
+    const [isopen , setIsOpen]= useState(false)
     return (
         <nav>
             <Link to="/" className='title'>React Nav</Link>
-            <div className="menu">
+            <div className="menu" onClick={()=>setIsOpen(!isopen)}>
                 <span></span>
                 <span></span>
                 <span></span>
             </div>
-            <ul>
+            <ul className={isopen ? "open" : ""}>
                 <li><NavLink to="Home">Home</NavLink></li>
                 <li><NavLink to="/about">About</NavLink></li>
                 <li><NavLink to="/services">Services</NavLink></li>
